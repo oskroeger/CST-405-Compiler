0331598c739417623358329af42169abbc378663
oskroeger <okroeger2@gmail.com>
2024-12-24 12:10:39 -0700

    Loop compiler backup

---
+++ loop_compiler/codeGenerator.h
@@ -0,0 +1,11 @@
+#ifndef CODE_GENERATOR_H
+#define CODE_GENERATOR_H
+
+#include "semantic.h"
+#include "symbolTable.h"
+#include <stdio.h>
+
+// Function to generate MIPS assembly code from TAC
+void generateMIPSFromTAC(TAC* tacHead, ScopeNode* scopeListHead, const char* outputFilename);
+
+#endif // CODE_GENERATOR_H
